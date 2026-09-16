a7199a8699395ee5759f7b51aeb95a393fcfa7b0
ym9079389-alt <ym9079389@gmail.com>
2026-09-16 15:23:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assigment.Net.OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
