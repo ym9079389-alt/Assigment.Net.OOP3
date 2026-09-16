aa3101402ffe230e2af9c84e412be75dbfb44f57
ym9079389-alt <ym9079389@gmail.com>
2026-09-16 17:06:37 +0300

    Q 1 => a

--- Program.cs
+++ Program.cs
@@ -4,6 +4,17 @@
     {
         static void Main(string[] args)
         {
+            #region Q 1
+
+            #region a
+            //a)  What is the difference between Method Overloading and Method Overriding?
+            //The purpose of method overloading is to reuse the function in more than one way,
+            //while method overriding is to change the base function in more than one place.
+            #endregion
+
+
+
+            #endregion
         }
     }
 }
